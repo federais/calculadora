30d542a2a226f4d53d21fc019d289023063ccac6
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2021-09-14 15:54:24 -0300

    add redirects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Route from './router'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './reset.css'
 
 import 'react-toastify/dist/ReactToastify.css'
@@ -9,10 +9,10 @@ import { ToastContainer } from 'react-toastify'
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Route path='/' />
         <ToastContainer autoClose={3000} position='top-left' />
-      </HashRouter>
+      </BrowserRouter>
     </>
   )
 
